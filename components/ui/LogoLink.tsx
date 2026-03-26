58ad2292e2a514925e6465e91304f34c1153c5b9
Óscar Jiménez <devlabsgt@gmail.com>
2026-03-26 15:07:45 -0600

    tamanio

--- components/ui/LogoLink.tsx
+++ components/ui/LogoLink.tsx
@@ -42,7 +42,7 @@ export default function LogoLink() {
           }}
         >
           <h1
-            className="font-serif text-[9px] md:text-2xl font-bold leading-tight text-left mt-5 bg-gradient-to-r from-blue-800 via-blue-400 to-blue-800 bg-[length:200%_auto] text-transparent bg-clip-text animate-text-shine"
+            className="font-serif text-sm md:text-2xl font-bold leading-tight text-left mt-5 bg-gradient-to-r from-blue-800 via-blue-400 to-blue-800 bg-[length:200%_auto] text-transparent bg-clip-text animate-text-shine"
             style={{ fontFamily: "'DM Serif Display', serif" }}
           >
             Sistema de Organización <br /> Territorial Estratégica
